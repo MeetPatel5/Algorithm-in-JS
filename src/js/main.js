62e8ecac279e7df1c16a515d03b0ff31453fec0d
MeetPatel5 <meet.patel@openxcellinc.com>
2018-10-11 17:14:07 +0530

    main.js updated

--- src/js/main.js
+++ src/js/main.js
@@ -35,3 +35,30 @@ console.clear();
 // 5. Array Chunk
 import { chunkArr } from "./array-chunking/array-chunking";
 console.log(chunkArr([1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.clear();
+
+// 6. Unique Array
+import { uniqueArr } from "./unique-array/unique-array";
+console.log(uniqueArr([1, 1, 2, 3, 3, 4, 5, 5, 5, 5, 6, 6]));
+console.clear();
+
+// 7. Array Sorting
+import {
+  sortByAsce,
+  sortByDesc,
+  sortByGreaterValue,
+  sortBySmallerValue
+} from "./array-sort/common-sort";
+// console.log(sortByAsce(["hi", "bye", "old", "whatever", 5, true])); => will throw an err as there are non string type of ele are presents
+// console.log(sortByDesc(["hi", "bye", "new", "old", "whatever", 2])); => will throw an err as there are non string type of ele are presents
+console.log(sortByAsce(["hi", "bye", "old", "whatever"]));
+console.log(sortByDesc(["hi", "bye", "new", "old", "whatever"]));
+
+// console.log(
+//   sortByGreaterValue([100, "25", 4165, true, 6565, 654, 4458, 66, 448])
+// ); =>  will throw an err as there are non number type of  ele are presents
+// console.log(
+//   sortBySmallerValue([564, "55", false, 4563, 31, 25, 45, 6541, 54165])
+// );  =>  will throw an err as there are non number type of  ele are presents
+console.log(sortByGreaterValue([100, 25, 4165, 12, 6565, 654, 4458, 66, 448]));
+console.log(sortBySmallerValue([564, 55, 654, 4563, 31, 25, 45, 6541, 54165]));
