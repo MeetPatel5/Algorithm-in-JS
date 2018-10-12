6023055bca2520b17d7f2ac642f2518776f6985d
MeetPatel5 <meet.patel@openxcellinc.com>
2018-10-12 08:39:06 +0530

    comment updated !

--- src/js/unique-array/unique-array.js
+++ src/js/unique-array/unique-array.js
@@ -3,6 +3,7 @@ export const uniqueArr = array => {
   return array.filter((ele, i) => {
     // console.log(`${array.indexOf(ele)} ? === ${i}`); => this is what happen under the hood
     // if return true , then keep it else remove it
+    // it'll keep all the same value having different types
     return array.indexOf(ele) === i;
   });
 };
